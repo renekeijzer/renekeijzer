6e2aa2f35dcf50a0aea75a6315eda968aa88ec2f
Rene <rkeijzer24@gmail.com>
2017-09-05 15:43:43 +0200

    some changes to colors

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,5 +9,15 @@
  */
 angular.module('renekeijzerApp')
   .controller('MainCtrl', function ($scope) {
-
+    $(window).scroll(function () {
+      if( $(window).scrollTop() > $('#head-main').offset().top && !($('#head-main').hasClass('head-sticky'))){
+        $('#head-main').addClass('head-sticky');
+        $('#head-main').removeClass('has-img');
+        $('#head-main').css("min-height", "60px");
+      } else if ($(window).scrollTop() == 0){
+        $('#head-main').removeClass('head-sticky');
+        $('#head-main').addClass('has-img');
+        $('#head-main').css("min-height", "0px");
+      }
+    });
   });
