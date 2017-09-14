ec87e1fb8bfccca53b3a2eb925f1b6db6d834558
rene keijzer <rene.keijzer@casema.nl>
2017-09-15 00:16:26 +0200

    more

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,8 @@
  * Controller of the renekeijzerApp
  */
 angular.module('renekeijzerApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $timeout) {
+    var $this = this;
     $scope.skillsVisible = false;
     $(window).scroll(function () {
       if( $(window).scrollTop() > $('#head-main').offset().top && !($('#head-main').hasClass('head-sticky'))){
@@ -30,7 +31,24 @@ angular.module('renekeijzerApp')
     $scope.animateExperience = function animateExperience() {
       $('#experienceD').removeClass('n-animate-up');
       $('#experienceD').addClass('animate-up');
+      $('.timeline-inner > .timeline-box').each(function(){
+        $this.handleElement(this, 250);
+
+      });
     }
+    $this.handleElement = function handleElement(element, time){
+      $timeout(function(){
+      var box = $(element).children()[1];
+      if($(box).hasClass('n-animate-left')){
+        $(box).removeClass('n-animate-left');
+        $(box).addClass('animate-left');
+      }else if ($(box).hasClass('n-animate-right')) {
+        $(box).removeClass('n-animate-right');
+        $(box).addClass('animate-right');
+      }
+    }, time);
+    }
+
 
     $scope.animateBlog = function animateBlog() {
       $('#blogD').removeClass('n-animate-up');
